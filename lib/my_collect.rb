8a359ae5f2517f2323e393fde64b1fb7f24ee74b
Jeffrey Yu <jwy5140@gmail.com>
2019-02-22 05:28:35 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+def my_collect(arr)
+  n = 0 
+  arr2 = Array.new
+  while n < arr.length do
+    if (arr[n].include?(" "))
+      arr2.push(yield (arr[n].split(" ")).first)
+    elsif (!arr.empty?)
+      arr2.push(yield arr[n].upcase)
+    end
+    n += 1 
+  end
+  arr2
+end
+
+
 
 
