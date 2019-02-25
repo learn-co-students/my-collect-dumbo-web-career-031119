59d6e45471d6fa711ee6750197b796eeafd7e89e
Jordan R <jordan.rivera34@gmail.com>
2019-02-25 13:53:25 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
+def my_collect(array)
+    nuarray = []
+    i = 0
+    while i < array.length
+      nuarray << yield(array[i])
+      i += 1
+    end
+     nuarray 
+  end
 
+array = ["Tim Jones", "Tom Smith", "Sophie Johnson", "Antoin Miller"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
 
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
