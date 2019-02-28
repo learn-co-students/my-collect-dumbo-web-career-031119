3d1974de52d60c83dee3b4ceffd16923a2d395ad
Jerry <jvr572@Gmail.com>
2019-02-28 16:13:38 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+def my_collect(collection)
+  i = 0
+  arr = []
+  while i < collection.length
+    arr.push(yield(collection[i]))
+    i += 1
+  end
 
+    collection.collect do |name|
+     name.split(" ").first
+    end
 
+  collection.collect do |lang|
+    lang.upcase
+  end
+
+  arr
+end
